293f075ac742c1a617ba36db4582edd663937713
logic <logicfeel@nate.com>
2023-03-28 19:23:44 +0900

    test c 까지 구성

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * ES5, CJS, NS
+ */
+const { BindModel } = require('./src/bind-model');
+const { BindModelAjax } = require('./src/bind-model-ajax');
+
+exports.BindModel = BindModel;
+exports.BindModelAjax = BindModelAjax;

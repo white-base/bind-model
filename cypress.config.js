b0b5ef3c9f1958f8d1ac9aa4e28f87045762f030
logic <logicfeel@nate.com>
2023-04-10 14:58:12 +0900

    cypress 메인테스트

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    e2e: {
-      baseUrl: 'http://localhost:8080/',
-    },
+    // baseUrl: 'http://localhost:8080',
   },
-});
+})

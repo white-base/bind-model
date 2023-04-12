61d7e44ec6363e55fe7102a756b18093b6e49926
logic <logicfeel@nate.com>
2023-04-12 13:54:02 +0900

    돔으로 내용 수정 확인

--- temp/todo.test.js
+++ temp/todo.test.js
@@ -4,17 +4,17 @@
 'use strict';
 
 // jsdom 브라우저 출력
-// const express = require('express');
-// const { TextEncoder, TextDecoder } = require('util');
-// global.TextEncoder = TextEncoder;
-// global.TextDecoder = TextDecoder;
-// const { JSDOM } = require('jsdom');
+const express = require('express');
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+const { JSDOM } = require('jsdom');
 
 test('Check addTodo able add todo to todoList', () => {
     document.body.innerHTML = `
-      <input id="newTodoInput" />
-      <button id="addTodoBtn">Add todo</button>
-      <ol id="todoList"></ol>
+    <input id="newTodoInput" />
+    <button id="addTodoBtn">Add todo</button>
+    <ol id="todoList"></ol>
     `;
     require('./todoList');
     // const $ = require( "jquery" )( window );
@@ -24,28 +24,31 @@ test('Check addTodo able add todo to todoList', () => {
     const addTodoBtn = document.getElementById('addTodoBtn');
     const todolist = document.getElementById('todoList');
 
-  newTodoInput.value = 'New todolist!';
-  addTodoBtn.click();
+    newTodoInput.value = 'New todolist!';
+    addTodoBtn.click();
 
-  expect(todolist.innerHTML).toBe('<li>New todolist!</li>');
+    newTodoInput.value = 'New todolist2';
+    addTodoBtn.click();
 
-  // jsdom 브라우저 출력
-  // const dom = new JSDOM(`<!DOCTYPE html><body><p id="main">My First JSDOM!</p></body>`,
-  //     {
-  //       url: "http://localhost:8000/",
-  //       contentType: "text/html",
-  //       pretendToBeVisual: true,
-  //     }
-  // );
-  // const app = express();
+    // expect(todolist.innerHTML).toBe('<li>New todolist!</li>');
 
-  // app.get('/', (req, res) => {
-  //   res.send(dom.serialize());
-  // });
+    // jsdom 브라우저 출력
+    const dom = new JSDOM(`<!DOCTYPE html><body>${document.body.innerHTML}</body>`,
+        {
+            url: "http://localhost:8000/",
+            contentType: "text/html",
+            pretendToBeVisual: true,
+        }
+    );
+    const app = express();
 
-  // app.listen(8080, () => {
-  //   console.log('Example app listening at http://localhost:8080');
-  // });
+    app.get('/', (req, res) => {
+        res.send(dom.serialize());
+    });
+
+    app.listen(8080, () => {
+        console.log('Example app listening at http://localhost:8080');
+    });
 
 });
 

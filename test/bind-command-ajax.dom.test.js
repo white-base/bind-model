3fdae3be33a9f850cb076acbdcefdfdaba337fae
logicfeel <logicfeel@gmail.com>
2024-06-12 18:46:50 +0900

    커밋

--- test/bind-command-ajax.dom.test.js
+++ test/bind-command-ajax.dom.test.js
@@ -26,7 +26,7 @@ const  axios  = require("axios");
 jest.mock('axios');
 
 const T = true;
-// let MetaObjectSub, MetaElementSub, ComplexElementSub, EmpytClass;
+// let MetaObjectSub, MetaElementSub, ComplexElementSub, EmpytClass; 
 
 //==============================================================
 // test
